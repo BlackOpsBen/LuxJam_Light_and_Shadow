d75117b2421e03e119307f6731234457410ed1dd
Ben Mora <blackopsben@gmail.com>
2020-03-07 10:49:25 -0600

    Torch illumination works

--- Assets/Illuminate.cs
+++ Assets/Illuminate.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Illuminate : MonoBehaviour
 {
+    [SerializeField] Image torchSprite;
+    [SerializeField] RectTransform fuelMeterFill;
+    private float fuelMeterMaxSize = 32f;
+    private float fuelMeterCurrentSize;
+
     private GameObject darkness;
     private float maxFuel = 3f;
     private float fuel = 3f;
@@ -17,18 +23,32 @@ public class Illuminate : MonoBehaviour
 
     private void Update()
     {
+        GetUseTorch();
+        UpdateFuelMeter();
+    }
+
+    private void GetUseTorch()
+    {
         if (!disabled)
         {
             if (fuel > float.Epsilon && Input.GetButton("Light"))
             {
                 darkness.SetActive(false);
+                torchSprite.enabled = true;
                 fuel -= fuelConsumptionRate * Time.deltaTime;
             }
             else
             {
                 darkness.SetActive(true);
+                torchSprite.enabled = false;
             }
         }
+    }
+
+    private void UpdateFuelMeter()
+    {
+        fuelMeterCurrentSize = fuel * fuelMeterMaxSize / maxFuel;
+        fuelMeterFill.localScale = new Vector3(fuelMeterCurrentSize, 1f, 1f);
     }
 
     public void ResetFuel()
